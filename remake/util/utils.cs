e1a1dcc683fec2a8bc73cfc64aa03e32fc98d0d4
Hash <kxhashimoto@gmail.com>
2020-05-09 17:09:13 +0200

    Push missing methods (menu utils)

--- remake/util/utils.cs
+++ remake/util/utils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,11 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.IO;
 using MelonLoader;
+using UnhollowerRuntimeLib;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TestMod.remake.btn;
+using OVR.OpenVR;
 
 namespace TestMod.remake.util
 {
@@ -40,12 +45,12 @@ namespace TestMod.remake.util
                 MelonModLogger.Log("!!! There was a update for this mod !!!");
                 MelonModLogger.Log("!!! Please update the mod to enjoy new features and bug fixes !!!");
                 MelonModLogger.Log("https://github.com/kichiro1337/vrchat_useful_mod");
-                return false;
+                return true;
             }
             else
             {
                 MelonModLogger.Log("Mod is up to date!");
-                return true;
+                return false;
             }
         }
         public static VRCPlayer get_local()
@@ -116,6 +121,30 @@ namespace TestMod.remake.util
             var text = (!a.toggledOn) ? t.alternateText : t.text;
             if (TooltipManager.field_Private_Static_Text_0 != null) TooltipManager.Method_Public_Static_Void_String_2(text);
             if (t.tooltip != null) t.tooltip.text = text;
+        }
+        public static Text make_slider(GameObject parent, Action<float> act, int bx, int by, string text, float def, float max, float min, int negate)
+        {
+            var btn = btn_utils.create_btn(false, ButtonType.Default, "slider_element_" + bx + by, "", Color.white, Color.white, bx, by, parent.transform, null);  btn.SetActive(false);
+            var slider = UnityEngine.Object.Instantiate<Transform>(utils.get_ui_manager().menuContent.transform.Find("Screens/Settings/AudioDevicePanel/VolumeSlider"), parent.gameObject.transform);
+            slider.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            slider.transform.localPosition = btn.gameObject.transform.localPosition; slider.transform.localPosition -= new Vector3(0, negate);
+            slider.GetComponentInChildren<RectTransform>().anchorMin += new Vector2(0.06f, 0f);
+            slider.GetComponentInChildren<RectTransform>().anchorMax += new Vector2(0.1f, 0f);
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().onValueChanged = new UnityEngine.UI.Slider.SliderEvent();
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().value = def;
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().maxValue = max;
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().minValue = min;
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().onValueChanged.AddListener(DelegateSupport.ConvertDelegate<UnityAction<float>>(act));
+            slider.GetComponentInChildren<UnityEngine.UI.Slider>().UpdateVisuals();
+            var txt = new GameObject("Text"); txt.transform.SetParent(parent.transform, false);
+            var txt_component = txt.AddComponent<Text>();
+            txt_component.font = Resources.GetBuiltinResource<Font>("Arial.ttf"); txt_component.fontSize = 64; txt_component.text = text;
+            txt_component.transform.localPosition = slider.transform.localPosition;
+            txt_component.transform.localPosition += new Vector3(txt_component.fontSize * text.Count()/5, 75);
+            txt_component.enabled = true;
+            txt_component.GetComponent<RectTransform>().sizeDelta = new Vector2(txt_component.fontSize * text.Count(), 100);
+            txt_component.alignment = TextAnchor.MiddleLeft;
+            return txt_component;
         }
     }
 }
